c0076d07aea3273d47aad920272993018729484b
Darren Shankland <ds@4pq.co.uk>
2019-04-29 16:34:35 +0100

    done mvp, trying extensions

--- src/app.js
+++ src/app.js
@@ -4,9 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
 	new Vue({
 		el: "#app",
 		data: {
-
+			todos: [
+				// "Buy shopping", "Clean bathroom", "Car's MOT"
+				{ todo: "Buy shopping", priority: "High" },
+				{ todo: "Clean bathroom", priority: "Low" },
+				{ todo: "Car's MOT", priority: "Low" },
+			],
+			newItem: ""
 		},
 		methods: {
+
+			saveNewItem: function(){
+        // this.todos.push(this.newItem);
+				this.todos.push({
+					todo: this.newItem,
+					purchased: "Low"
+				});
+				this.newItem = "";
+			}
 
 		}
 	});
